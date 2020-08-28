1c1ee3c3f2e548c9dc83b13c642d13ab704f1b51
mtrepanier935 <MichaelMDTrep@Gmail.com>
2020-08-28 00:45:27 -0400

    second commit

--- morse.py
+++ morse.py
@@ -9,24 +9,47 @@ Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
-__author__ = '???'
+__author__ = 'Michael Trepanier'
 
 from morse_dict import MORSE_2_ASCII
 
 
 def decode_bits(bits):
-    # your code here
-    return
+    m = []
+    mult = 2
+    bits = ("""110011001100110000001100000011111100110011111100111111
+    0000000000000011001111110011111100111111000000110011
+    001111110000001111110011001100000011""")
+
+    for bit in bits:
+        if bit.startswith("0000000"):
+            bits.replace("0000000"*mult, " ")
+        if bit.startswith("111"):
+            bits.replace("111"*mult, "-")
+        if bit.startswith("000"):
+            bits.replace("000"*mult, " ")
+        if bit.startswith("1"):
+            bits.replace("1"*mult, ".")
+        if bit.startswith("0"):
+            bits.replace("0"*mult, "")
+
+    return (sorted((filter(None, bits))))
 
 
 def decode_morse(morse):
-    # your code here
-    return
+    """this function translates morse code into strings"""
+    result = []
+    for word in morse.strip().split("   "):
+        ascii_word = []
+        for morse_char in word.split(" "):
+            ascii_word.append(MORSE_2_ASCII[morse_char])
+        result.append("".join(ascii_word))
+    return " ".join(result)
 
 
 if __name__ == '__main__':
     hey_jude_morse = ".... . -.--   .--- ..- -.. ."
-    hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011" # noqa
+    hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"  # noqa
 
     # Be sure to run all included unit tests, not just this one.
     print("Morse Code decoder test")
