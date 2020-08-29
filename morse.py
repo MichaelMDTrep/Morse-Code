640277f81964f3538070acba916e7058249c17a2
mtrepanier935 <MichaelMDTrep@Gmail.com>
2020-08-29 00:57:37 -0400

    second commit

--- morse.py
+++ morse.py
@@ -9,7 +9,7 @@ Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
-__author__ = 'Michael Trepanier got help from piero'
+__author__ = 'Michael Trepanier got help from piero with the first part'
 
 from morse_dict import MORSE_2_ASCII
 
